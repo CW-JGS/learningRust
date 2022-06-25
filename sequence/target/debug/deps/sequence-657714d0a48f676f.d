a3d4e5af4bf8b3e981160cded549da6167e80f08
CW-JGS <103899285@swin.edu.au>
2022-06-25 13:07:27 +1000

    first exercise doneski

---
+++ sequence/target/debug/deps/sequence-657714d0a48f676f.d
@@ -0,0 +1,5 @@
+C:\Users\jacks\Desktop\projects\learningRust\sequence\target\debug\deps\sequence-657714d0a48f676f.exe: src\main.rs
+
+C:\Users\jacks\Desktop\projects\learningRust\sequence\target\debug\deps\sequence-657714d0a48f676f.d: src\main.rs
+
+src\main.rs:
---
+++ sequence/target/debug/deps/sequence-c4aaa811ec8d03af.d
@@ -0,0 +1,5 @@
+c:\Users\jacks\Desktop\projects\learningRust\sequence\target\debug\deps\sequence-c4aaa811ec8d03af.rmeta: src\main.rs
+
+c:\Users\jacks\Desktop\projects\learningRust\sequence\target\debug\deps\sequence-c4aaa811ec8d03af.d: src\main.rs
+
+src\main.rs:
